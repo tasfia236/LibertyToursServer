d75620b9264f8a3f1cdeb0f8f26036e46f700212
tasfia236 <tasfiatabassum300@gmail.com>
2024-05-02 17:00:42 +0600

    index modify for deploy

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-
 const port = process.env.PORT || 8000;
 
-// middleware
-app.use(cors());
+//middleware
+//app.use(cors());
+app.use(cors({origin: ['https://liberty-tours-70cb2.web.app/','https://liberty-tours-server.vercel.app','http://localhost:8080']}))
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxwisw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxwisw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,20 +21,21 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        client.connect()
+        // await client.connect();
+
         const tourspotCollection = client.db('tourspotsDB').collection('tourspots');
-        const countryCollection = client.db("tourspotsDB").collection("countries");
-        
+        const countryCollection = client.db('tourspotsDB').collection('countries');
+
         app.get('/tourspots', async (req, res) => {
             const cursor = tourspotCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        
         app.get('/tourspots/:id', async (req, res) => {
             const id = req.params.id;
             const quary = { _id: new ObjectId(id) }
@@ -106,22 +107,22 @@ async function run() {
             const result = await tourspotCollection.deleteOne(quary);
             res.send(result);
         })
-        
-        
-        app.get('/', (req, res) => {
-            res.send('running');
-        })
 
-        
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged... You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        //  await client.close();
     }
 }
 run().catch(console.dir);
 
-
+app.get('/', (req, res) => {
+    res.send('Travelling Spots!!')
+})
 
 app.listen(port, () => {
-    console.log(`brand server is running on port ${port}`);
+    console.log(`Example app listening on port ${port}`)
 })
